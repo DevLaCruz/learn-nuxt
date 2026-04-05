5b75164577f4176484a5bcddf76fa94ce483197d
DevLaCruz <alejantony.22@hotmail.com>
2026-04-05 15:50:47 -0500

    first commit

---
+++ first-site/app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Hello Word</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quas itaque voluptates similique aspernatur iste doloremque vitae. Deserunt error placeat obcaecati earum corrupti iusto, odit at iure blanditiis eius debitis?</p>
+  </div>
+</template>
